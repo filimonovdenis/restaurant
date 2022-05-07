eab4f74666255c55d9f6aaa826d25217d4efe12d
filimonovdenis <78161279+filimonovdenis@users.noreply.github.com>
2022-05-07 17:04:54 +0300

    rest

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+$(document).ready(function() {
+$(".header_burger").click(function () {
+$(".container").toggleClass("shadow");
+});
+});
+
+
+
+
+$(document).ready(function() {
+	$('.header_burger').click(function(event) {
+		$('.header_burger,.header_menu').toggleClass('active');
+		$('body').toggleClass('lock');
+		$('body')
+
+	});
+});
+
+
+
+if (document.documentElement.clientWidth > 768) {
+
+}
+
+
+/*дальше мой новый код*/
+
